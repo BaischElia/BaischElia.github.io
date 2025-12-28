31b365fc98dd0442f00fd3c495269aaec320e1fe
DESKTOP-TVMP782\Elia <eb083@hdm-stuttgart.de>
2025-12-28 17:48:28 +0100

    Updated

--- script/typed.js
+++ script/typed.js
@@ -1,5 +1,5 @@
 let typed = new Typed('.typing', {
-    strings: ['Elia Baisch','Web Developer', 'UI/UX Designer', 'Application Developer', 'Graphic Designer', 'Visual Designer', 'Project Manager', 'Concept Developer'],
+    strings: ['Elia Baisch','UI/UX Designer', 'Web Developer', 'Software Developer', 'Application Developer', 'Graphic Designer', 'Visual Designer', 'Project Manager', 'Concept Developer'],
     typeSpeed: 100,
      backSpeed: 100,
      loop: true
